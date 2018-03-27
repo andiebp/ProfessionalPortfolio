ed5c46b1cfb8adf5cce1969dd7a06ca046c40f35
andiebp <andiepatel@gmail.com>
2018-03-26 22:48:23 -0500

    Added project 1 and added Materialize accordion functionality

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,70 +1,40 @@
 import React from "react";
+import {Route} from "react-router-dom";
 
-const Page = () => (<div>
-	<div id="publications" className="section scrollspy">
-		<div className="container center">
-			<div className="row">
-				<div className="col s12 light center header">
-					<h4 className="header center-on-small-only">Publications</h4>
+class Page extends React.Component {
+	componentDidMount() {
+		let elem = document.querySelector('.collapsible');
+		M.Collapsible.init(elem);
+	}
+	render() {
+		return (<div>
+			<div id="publications" className="section scrollspy">
+				<div className="container center">
+					<div className="row">
+						<div className="col s12 light center header">
+							<h4 className="header center-on-small-only">Thoughts Tornado</h4>
+							<ul className="collapsible" data-collapsible="accordion">
+								<li>
+									<div className="collapsible-header waves-effect waves-light teal lighten-2 white-text">Blog Post #1</div>
+									<div className="collapsible-body">
+										<p>Lorem ipsum dolor sit amet.</p>
+									</div>
+								</li>
+								<li>
+									<div className="collapsible-header waves-effect waves-light teal lighten-2 white-text">Blog Post #2</div>
+									<div className="collapsible-body">
+										<p>Lorem ipsum dolor sit amet.</p>
+									</div>
+								</li>
+							</ul>
+						</div>
 
-					<div id="workshops" className="sub-section">
-						<p>Workshops :</p>
-						<ul className="collapsible" data-collapsible="accordion">
-							<li>
-								<div className="collapsible-header waves-effect waves-light teal lighten-2 white-text">Workshop 2</div>
-								<div className="collapsible-body">
-									<p>Lorem ipsum dolor sit amet.</p>
-								</div>
-							</li>
-							<li>
-								<div className="collapsible-header waves-effect waves-light teal lighten-2 white-text">Workshop 1</div>
-								<div className="collapsible-body">
-									<p>Lorem ipsum dolor sit amet.</p>
-								</div>
-							</li>
-						</ul>
 					</div>
-
-					<div id="journels" className="sub-section">
-						<p>Journals :</p>
-						<ul className="collapsible" data-collapsible="accordion">
-							<li>
-								<div className="collapsible-header waves-effect waves-light teal lighten-2 white-text">Journal 2</div>
-								<div className="collapsible-body">
-									<p>Lorem ipsum dolor sit amet.</p>
-								</div>
-							</li>
-							<li>
-								<div className="collapsible-header waves-effect waves-light teal lighten-2 white-text">Journal 1</div>
-								<div className="collapsible-body">
-									<p>Lorem ipsum dolor sit amet.</p>
-								</div>
-							</li>
-						</ul>
-					</div>
-
-					<div id="conferences" className="sub-section">
-						<p>Conferences :</p>
-						<ul className="collapsible" data-collapsible="accordion">
-							<li>
-								<div className="collapsible-header waves-effect waves-light teal lighten-2 white-text">Conference 2</div>
-								<div className="collapsible-body">
-									<p>Lorem ipsum dolor sit amet.</p>
-								</div>
-							</li>
-							<li>
-								<div className="collapsible-header waves-effect waves-light teal lighten-2 white-text">Conference 1</div>
-								<div className="collapsible-body">
-									<p>Lorem ipsum dolor sit amet.</p>
-								</div>
-							</li>
-						</ul>
-					</div>
+					<div className="col s2 pull-s2"></div>
 				</div>
-				<div className="col s2 pull-s2"></div>
 			</div>
-		</div>
-	</div>
-</div>);
+		</div>);
+	}
+};
 
 export default Page;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,75 +1,57 @@
 import React from "react";
+import {Route} from "react-router-dom";
 
-const Page = () => (<div>
-	<div id="projects" class="section scrollspy">
-		<div class="container center">
-			<div class="row">
-				<div class="col s12 light center header">
-					<h4 class="header center-on-small-only">Projects</h4>
-
-					<div id="industrial" class="sub-section">
-						<p>Industrial :</p>
-						<ul class="collapsible" data-collapsible="accordion">
-							<li>
-								<div class="collapsible-header waves-effect waves-light teal lighten-2 white-text">Project 4</div>
-								<div class="collapsible-body">
-									<p>Lorem ipsum dolor sit amet.</p>
-								</div>
-							</li>
-							<li>
-								<div class="collapsible-header waves-effect waves-light teal lighten-2 white-text">Project 3</div>
-								<div class="collapsible-body">
-									<p>Lorem ipsum dolor sit amet.</p>
-								</div>
-							</li>
-							<li>
-								<div class="collapsible-header waves-effect waves-light teal lighten-2 white-text">Project 2</div>
-								<div class="collapsible-body">
-									<p>Lorem ipsum dolor sit amet.</p>
-								</div>
-							</li>
-							<li>
-								<div class="collapsible-header waves-effect waves-light teal lighten-2 white-text">Project 1</div>
-								<div class="collapsible-body">
-									<p>Lorem ipsum dolor sit amet.</p>
-								</div>
-							</li>
-						</ul>
-					</div>
-
-					<div id="research" class="sub-section">
-						<p>Research :</p>
-						<ul class="collapsible" data-collapsible="accordion">
-							<li>
-								<div class="collapsible-header waves-effect waves-light teal lighten-2 white-text">Project 4</div>
-								<div class="collapsible-body">
-									<p>Lorem ipsum dolor sit amet.</p>
-								</div>
-							</li>
-							<li>
-								<div class="collapsible-header waves-effect waves-light teal lighten-2 white-text">Project 3</div>
-								<div class="collapsible-body">
-									<p>Lorem ipsum dolor sit amet.</p>
-								</div>
-							</li>
-							<li>
-								<div class="collapsible-header waves-effect waves-light teal lighten-2 white-text">Project 2</div>
-								<div class="collapsible-body">
-									<p>Lorem ipsum dolor sit amet.</p>
-								</div>
-							</li>
-							<li>
-								<div class="collapsible-header waves-effect waves-light teal lighten-2 white-text">Project 1</div>
-								<div class="collapsible-body">
-									<p>Lorem ipsum dolor sit amet.</p>
-								</div>
-							</li>
-						</ul>
+class Page extends React.Component {
+	componentDidMount() {
+		let elem = document.querySelector('.collapsible');
+		M.Collapsible.init(elem);
+	}
+	render() {
+		return (<div>
+			<div id="projects" className="section scrollspy">
+				<div className="container center">
+					<div className="row">
+						<div className="col s12 light center header">
+							<h4 className="header center-on-small-only">My Projects</h4>
+							<ul className="collapsible" data-collapsible="accordion">
+								<li>
+									<div className="collapsible-header waves-effect waves-light teal lighten-2 white-text">Truax Trumpets
+									</div>
+									<div className="collapsible-body">
+										<div>
+											Placeholder for screenshot of project with&nbsp;
+											<a href="https://truax-trumpets.herokuapp.com/" target="_blank">
+												link
+											</a>.
+										</div>
+										<p>Placeholder for description of project</p>
+									</div>
+								</li>
+								<li>
+									<div className="collapsible-header waves-effect waves-light teal lighten-2 white-text">Project 3</div>
+									<div className="collapsible-body">
+										<p>Lorem ipsum dolor sit amet.</p>
+									</div>
+								</li>
+								<li>
+									<div className="collapsible-header waves-effect waves-light teal lighten-2 white-text">Project 2</div>
+									<div className="collapsible-body">
+										<p>Lorem ipsum dolor sit amet.</p>
+									</div>
+								</li>
+								<li>
+									<div className="collapsible-header waves-effect waves-light teal lighten-2 white-text">Project 1</div>
+									<div className="collapsible-body">
+										<p>Lorem ipsum dolor sit amet.</p>
+									</div>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	</div>
-</div>);
+		</div>);
+	}
+};
 
 export default Page;
